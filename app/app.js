5e74bdf24db29d71d1b4ab6adf22a4006b299298
Lawrence Nyakiso <lawrencenyakiso@Lawrences-MacBook-Pro.local>
2017-05-18 11:20:24 +0200

    Added Jira Support. Added Node Server as the middle man.

--- app/app.js
+++ app/app.js
@@ -2,12 +2,26 @@
 
 // Declare app level module which depends on views, and components
 angular.module('secDash', [
-  'ngMaterial',
-  'ngRoute',
-  'secDash.dashboard',
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngMaterial',
+    'ngRoute',
+    'secDash.dashboard',
+    'secDash.jira',
+    'secDash.countries'
+]).config(['$locationProvider', '$routeProvider','$httpProvider','$sceDelegateProvider', function ($locationProvider, $routeProvider, $httpProvider, $sceDelegateProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/dashboard'});
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
+
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.defaults.headers.common.Accept = 'application/json;charset=UTF-8';
+    $httpProvider.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8';
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+        // Allow loading from our assets domain.  Notice the difference between * and **.
+        'http://*.standardbank.co.za:8091/**',
+    ]);
+
 }]);
